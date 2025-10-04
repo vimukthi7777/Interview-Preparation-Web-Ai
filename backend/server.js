0cf752bbf4955e0bf4c6d02664b086eeea9b3bc2
Sahan Vimukthi <svimukthi7777@gmail.com>
2025-10-04 10:10:12 +0530

    chore(database): setup MongoDB connection with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const path = require("path");
+// const cors = require("cors");
+// const path = require("path");
 const connectDB = require("./config/db");
 
-const authRoutes = require("./routes/authRoutes")
-const resumeRoutes = require("./routes/resumeRoutes")
+// const authRoutes = require("./routes/authRoutes")
+// const resumeRoutes = require("./routes/resumeRoutes")
 
 const app = express();
 
 //Middleware to handle CORS
 
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL || "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"]
-    })
-);
+// app.use(
+//     cors({
+//         origin: process.env.CLIENT_URL || "*",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         allowedHeaders: ["Content-Type", "Authorization"]
+//     })
+// );
 
 //Connect Database
 connectDB();
@@ -26,18 +26,18 @@ connectDB();
 app.use(express.json());
 
 //Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/resume", resumeRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/resume", resumeRoutes);
 
 //Serve uploads folder
-app.use(
-    "/uploads",
-    express.static(path.join(__dirname, "uploads"), {
-        setHeaders: (res, path) => {
-            res.set("Access-Control-Allow-Origin", "http://localhost:5173");
-        },
-    })
-);
+// app.use(
+//     "/uploads",
+//     express.static(path.join(__dirname, "uploads"), {
+//         setHeaders: (res, path) => {
+//             res.set("Access-Control-Allow-Origin", "http://localhost:5173");
+//         },
+//     })
+// );
 
 
 //Start Server
