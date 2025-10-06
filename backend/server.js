b42dc81b0691e1b300f10cd79848fddc0860c71d
Sahan Vimukthi <svimukthi7777@gmail.com>
2025-10-06 10:14:14 +0530

    feat(auth): implement JWT-based user authentication with routes, controller, and middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-// const cors = require("cors");
-// const path = require("path");
+const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/db");
 
-// const authRoutes = require("./routes/authRoutes")
-// const resumeRoutes = require("./routes/resumeRoutes")
+const authRoutes = require("./routes/authRoutes")
 
 const app = express();
 
@@ -26,19 +25,15 @@ connectDB();
 app.use(express.json());
 
 //Routes
-// app.use("/api/auth", authRoutes);
-// app.use("/api/resume", resumeRoutes);
+app.use("/api/auth", authRoutes);
+// app.use("/api/sessions", sessionRoutes);
+// app.use("/api/questions", questionRoutes);
+
+// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 //Serve uploads folder
-// app.use(
-//     "/uploads",
-//     express.static(path.join(__dirname, "uploads"), {
-//         setHeaders: (res, path) => {
-//             res.set("Access-Control-Allow-Origin", "http://localhost:5173");
-//         },
-//     })
-// );
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
 //Start Server
 const PORT = process.env.PORT || 5000;
