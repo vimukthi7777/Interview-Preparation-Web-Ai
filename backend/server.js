58408af71ee03f7d62a29a48690a7d9ee4803f67
Sahan Vimukthi <svimukthi7777@gmail.com>
2025-10-10 14:45:07 +0530

    feat(ai): add question routes, controllers, and Gemini AI integration with prompts

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes")
 const sessionRoutes = require("./routes/sessionRoutes")
+const questionRoutes = require("./routes/questionRoutes");
+const { protect } = require("./middlewares/authMiddleware");
+const { generateInterviewQuestions, generateConceptExplanation } = require("./controllers/aiController");
 
 const app = express();
 
@@ -28,10 +31,10 @@ app.use(express.json());
 //Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/sessions", sessionRoutes);
-// app.use("/api/questions", questionRoutes);
+app.use("/api/questions", questionRoutes);
 
-// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
-// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
+app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 //Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
